f89258a6f81184957d99027d351d31de3d57a2b9
nosteam595 <nosteam@bk.ru>
2026-03-10 22:28:36 +0300

    task completed 2

--- src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/model/User.java
+++ src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/model/User.java
@@ -34,7 +34,7 @@ public class User {
     @Column(name = "password", length = 100)
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
             name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
--- src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/repositories/PeopleRepository.java
+++ src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/repositories/PeopleRepository.java
@@ -2,10 +2,14 @@ package nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.repositories;
 
 import nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface PeopleRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByFirstName(String username);
+public interface PeopleRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.firstName = :username")
+    Optional<User> findByFirstName(@Param("username") String username);
 }
